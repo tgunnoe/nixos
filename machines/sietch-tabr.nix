2e6ef6c0b22673a29e4575dfc6a9dd274ad86c6f
Taylor <tgunnoe@gnu.lv>
2022-02-19 23:59:13 -0500

    Add pi4 machine sietch-tabr

---
+++ machines/sietch-tabr.nix
@@ -0,0 +1,37 @@
+{ self, lib, config, pkgs, suites, ... }:
+
+{
+  imports = ["${fetchTarball "https://github.com/NixOS/nixos-hardware/archive/8f1bf828d8606fe38a02df312cf14546ae200a72.tar.gz" }/raspberry-pi/4"]
+            ++ suites.goPlay;
+
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/NIXOS_SD";
+      fsType = "ext4";
+      options = [ "noatime" ];
+    };
+  };
+
+  networking = {
+    hostName = "sietch-tabr";
+    firewall.allowedTCPPorts = [ 8000 30000 ];
+    firewall.allowedUDPPorts = [ 30000 ];
+    useDHCP = false;
+    wireless = {
+      enable = true;
+      interfaces.wlan0.useDHCP = true;
+    };
+    networkmanager.enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    emacs-nox
+    git
+  ];
+
+  services.openssh.enable = true;
+
+  hardware.raspberry-pi."4".fkms-3d.enable = true;
+
+  hardware.pulseaudio.enable = true;
+}
