f9aac222a9201be8c43186695fd82b3c796ff95b
tgunnoe <t@gvno.net>
2022-03-04 22:28:57 -0500

    Disable extra steam stuff

--- overlays/steam.nix
+++ overlays/steam.nix
@@ -1,9 +1,9 @@
 final: prev: {
   steam = prev.steam.override {
-    extraPkgs = pkgs: with pkgs; [ pango harfbuzz libthai ];
-    extraProfile = ''
-      unset VK_ICD_FILENAMES
-      export VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/intel_icd.x86_64.json:/usr/share/vulkan/icd.d/intel_icd.i686.json
-    '';
+    # extraPkgs = pkgs: with pkgs; [ pango harfbuzz libthai ];
+    # extraProfile = ''
+    #   unset VK_ICD_FILENAMES
+    #   export VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/intel_icd.x86_64.json:/usr/share/vulkan/icd.d/intel_icd.i686.json
+    # '';
   };
 }
