9d81d13877805833231c3d90892bb7e74fadfea9
Taylor <tgunnoe@gnu.lv>
2022-04-17 12:29:08 -0400

    Update spring engine to beyond all reason

--- pkgs/games/spring/default.nix
+++ pkgs/games/spring/default.nix
@@ -29,19 +29,31 @@
 , srcs
 , withAI ? true # support for AI Interfaces and Skirmish AIs
 }:
-let inherit (srcs) spring; in
-
+let
+  barData = fetchFromGitHub {
+    owner = "beyond-all-reason";
+    repo = "Beyond-All-Reason";
+    rev = "23bca51494640a8a1a1fc2c4f1ab259291d8bcb4";
+    sha256 = "sha256-DJDef4YM31DzSYiXdskQxpyGlIKaMysObSBvQUbPNmA=";
+  };
+  barChobby = fetchFromGitHub {
+    owner = "beyond-all-reason";
+    repo = "BYAR-Chobby";
+    rev = "aa64e8d040f96001c97d55c283cae93c75d2ebeb";
+    sha256 = "sha256-DJDef4YM31DzSYiXdskQxpyGlIKaMysObSBvQUbPNmA=";
+  };
+in
 stdenv.mkDerivation rec {
   pname = "spring";
-  #  version = "105.0.1-${buildId}-g${shortRev}";
-  inherit (spring) version;
-  rev = "8581792eac65e07cbed182ccb1e90424ce3bd8fc";
+  version = "105.1.1-${buildId}-g${shortRev}";
+  #inherit (spring) version;
+  rev = "34d2eb7a14fa8f316b308a6c6945ae8fcb00f9ce";
   shortRev = builtins.substring 0 7 rev;
-  buildId = "1486";
+  buildId = "902";
 
   # taken from https://github.com/spring/spring/commits/maintenance
   src = fetchFromGitHub {
-    owner = "spring";
+    owner = "beyond-all-reason";
     repo = pname;
     inherit rev;
     sha256 = "sha256-OUiniYrTDUnMQkeb6BsqtUIMKMwC9JwR3WdXnB9qmxY=";
@@ -60,7 +72,6 @@ stdenv.mkDerivation rec {
     rm rts/build/cmake/FindGLEW.cmake
     echo "${version} maintenance" > VERSION
   '';
-
   cmakeFlags = [
     "-DCMAKE_BUILD_WITH_INSTALL_RPATH:BOOL=ON"
     "-DCMAKE_INSTALL_RPATH_USE_LINK_PATH:BOOL=ON"
@@ -97,6 +108,8 @@ stdenv.mkDerivation rec {
   postInstall = ''
     wrapProgram "$out/bin/spring" \
       --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath [ stdenv.cc.cc systemd ]}"
+    ln -s ${barData} $out/share/games/BAR.sdd
+    ln -s ${barChobby} $out/share/games/BYAR-Chobby.sdd
   '';
 
   meta = with lib; {
