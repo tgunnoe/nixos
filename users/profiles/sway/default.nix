527f1999dbcd1b5974c92d7250dc96531c31e750
tgunnoe <t@gvno.net>
2022-06-01 16:01:52 -0400

    Update volume controls

--- users/profiles/sway/default.nix
+++ users/profiles/sway/default.nix
@@ -6,23 +6,25 @@ let
     executable = true;
 
     text = ''
-  dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
-  systemctl --user stop pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
-  systemctl --user start pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
-      '';
+      dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
+      systemctl --user stop pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
+      systemctl --user start pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
+    '';
   };
   configure-gtk = pkgs.writeTextFile {
-      name = "configure-gtk";
-      destination = "/bin/configure-gtk";
-      executable = true;
-      text = let
+    name = "configure-gtk";
+    destination = "/bin/configure-gtk";
+    executable = true;
+    text =
+      let
         schema = pkgs.gsettings-desktop-schemas;
         datadir = "${schema}/share/gsettings-schemas/${schema.name}";
-      in ''
+      in
+      ''
         export XDG_DATA_DIRS=${datadir}:$XDG_DATA_DIRS
         gnome_schema=org.gnome.desktop.interface
         gsettings set $gnome_schema gtk-theme 'Dracula'
-        '';
+      '';
   };
 in
 {
@@ -40,11 +42,7 @@ in
     wl-clipboard
     dbus-sway-environment
     configure-gtk
-    glib
-    xdg_utils
-    xwayland
-    libnl
-    libpulseaudio
+    pamixer
 
     autotiling
     #flashfocus
@@ -128,7 +126,7 @@ in
         ];
       };
       fonts = {
-        names = [ "DejaVu Sans Mono" "FontAwesome5Free" ];
+        names = [ "DejaVu Sans Mono" "FontAwesome5Free" "Nerdfonts" ];
         style = "Bold Semi-Condensed";
         size = 12.0;
       };
@@ -235,9 +233,9 @@ in
 
           XF86MonBrightnessUp = "exec \"light -A 10; notify-send 'brightness up'\"";
           XF86MonBrightnessDown = "exec \"light -U 10; notify-send 'brightness down'\"";
-          XF86AudioMute = "exec pactl set-sink-mute @DEFAULT_SINK@ toggle";
-          XF86AudioRaiseVolume = "exec pactl set-sink-volume @DEFAULT_SINK@ +5%";
-          XF86AudioLowerVolume = "exec pactl set-sink-volume @DEFAULT_SINK@ -5%";
+          XF86AudioMute = "exec pamixer -t";
+          XF86AudioRaiseVolume = "exec pamixer -i 5 #to increase 5%";
+          XF86AudioLowerVolume = "exec pamixer -d 5 #to decrease 5%";
           "${modifier}+Shift+b" = "move left";
           "${modifier}+Shift+n" = "move down";
           "${modifier}+Shift+p" = "move up";
