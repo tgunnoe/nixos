00c44c5fc0557461c91ea3d6f39eceb1b537d60f
tgunnoe <t@gvno.net>
2022-08-16 11:54:25 -0400

    Update flake.lock

--- flake.nix
+++ flake.nix
@@ -6,6 +6,7 @@
     extra-substituters = "https://nrdxp.cachix.org https://nix-community.cachix.org https://hydra.iohk.io";
     extra-trusted-public-keys = "nrdxp.cachix.org-1:Fc5PSqY2Jm1TrWfm88l6cvGWwz3s93c6IOifQWnhNW4= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=";
     trusted-substituters = "http://hydra.nixos.org";
+    allow-import-from-derivation = true;
   };
   inputs =
     {
